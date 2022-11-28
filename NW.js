a600783e1a450a7415d3d9ed1fa60bb89939ff20
Larry Holst <larry.holst@disroot.org>
2022-11-28 21:55:53 +0300

    clean NW.js

--- NW.js
+++ NW.js
@@ -5,15 +5,6 @@
 	licensed under MIT License
 */
 
-/*
-It would also be necessary to add an option to tell the user that if the file name
-contains invalid characters, they will be replaced. In addition, if the file name
-length exceeds 256, it cannot be saved in the NTFS file system.
-
-To-Do:
-- Economy system (currency: RUB). A shop for privileges.
-*/
-
 // import
 function get_key_by_value(object, value) {
 	return Object.keys(object).find(key => object[key] === value)
